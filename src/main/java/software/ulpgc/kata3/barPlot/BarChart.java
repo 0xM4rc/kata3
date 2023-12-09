aeaea131f7503b2caf8270ab6f01ff874bc8f1d7
marc <marcos.sanchez2003@proton.me>
2023-12-09 18:48:52 +0000

    0

--- src/main/java/software/ulpgc/kata3/barPlot/BarChart.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.kata3.barPlot;
-
-
-import java.util.List;
-
-public record BarChart(List<Double> values, List<String> series, List<String> category) {
-}
--- src/main/java/software/ulpgc/kata3/barPlot/swing/BarChartDisplay.java
+++
@@ -1,7 +0,0 @@
-package software.ulpgc.kata3.barPlot.swing;
-
-import software.ulpgc.kata3.barPlot.BarChart;
-
-public interface BarChartDisplay {
-    void show(BarChart barChart);
-}
