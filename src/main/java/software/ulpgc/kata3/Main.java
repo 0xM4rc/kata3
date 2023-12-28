b6fd1306328c1cac17717846a77c8cc8067daf74
marc <marcos.sanchez2003@proton.me>
2023-12-28 13:21:11 +0000

    modified Main

--- src/main/java/software/ulpgc/kata3/Main.java
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -1,5 +1,7 @@
 package software.ulpgc.kata3;
 
+import software.ulpgc.kata3.barChart.BarChart;
+import software.ulpgc.kata3.barChart.swing.MainFrame;
 import software.ulpgc.kata3.electricVehicle.CsvFileElectricVehicleLoader;
 import software.ulpgc.kata3.electricVehicle.ElectricVehicle;
 import software.ulpgc.kata3.electricVehicle.ElectricVehicleStatistic;
@@ -19,6 +21,20 @@ public class Main {
 
         ElectricVehicleStatistic statistic = new YearsElectricVehicleStatic();
         Map<String, Integer> map = statistic.calculate(vehicles);
+
+        List<String> sortedYears = new ArrayList<>(map.keySet());
+        sortedYears.sort(null);
+
+        List<Double> sortedValues = new ArrayList<>();
+        for (String year: sortedYears){
+            sortedValues.add(Double.valueOf(map.get(year)));
+        }
+
+        MainFrame frame = new MainFrame();
+
+        frame.barChartDisplay().show(new BarChart(sortedValues, sortedYears, null));
+
+        frame.setVisible(true);
 
     }
 }
