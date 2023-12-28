6f4fd1481da4f6030af19099396178a63dcb8e31
marc <marcos.sanchez2003@proton.me>
2023-12-28 12:55:01 +0000

    added BarChart, BarChartDisplay, JFreeChartDisplay, MainFrame

---
+++ src/main/java/software/ulpgc/kata3/barChart/swing/MainFrame.java
@@ -0,0 +1,26 @@
+package software.ulpgc.kata3.barChart.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private JFreeChartDisplay barChartDisplay;
+
+    public MainFrame() throws HeadlessException {
+        setTitle("BarChart");
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        add(createBarChartDisplay());
+    }
+
+    private Component createBarChartDisplay() {
+        this.barChartDisplay = new JFreeChartDisplay();
+        return this.barChartDisplay;
+    }
+
+    public BarChartDisplay barChartDisplay(){
+        return this.barChartDisplay;
+    }
+}
